4a80eaaa6a9203279d4e0075ca6762e5e35d0ed5
Florante Rapio <florante.rapio@ing.com>
2022-03-06 20:17:27 +0800

    Updated margins

--- components/AboutThisWebsite.vue
+++ components/AboutThisWebsite.vue
@@ -2,9 +2,12 @@
   <section id="about-this-website" class="container f-center">
     
     <div class="white-card">
-        <p class="about-this-website-head">About this website</p>
+        <div class="margins">
+                <p class="about-this-website-head">About this website</p>
         <p>This website is created using NuxtJS and Deployed in Heroku.. So Far. This is in continuous development and I am planning to integrate a backend for this one.</p>
-        <p class="next-paragraph">you can find the source code for this one in GitHub <a href="https://github.com/plurantee/flogramming-nuxt" target="_blank">flogramming-nuxt</a></p>
+        <p class="next-paragraph">you can find the source code for this one in GitHub - <a href="https://github.com/plurantee/flogramming-nuxt" target="_blank">flogramming-nuxt.. this is a link btw</a></p>
+    
+        </div>
     </div>  
     
   </section>
@@ -30,6 +33,11 @@
     font-weight: 700;
     font-size: 2rem;
     margin-bottom: 30px;
+
+}
+.margins {
+
+    margin: 30px 70px 40px 70px;
 }
 </style>
 
